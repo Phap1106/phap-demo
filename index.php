fb61561498547d680a1ddf59be91df669813987a
Phap1106 <91425149+Phap1106@users.noreply.github.com>
2021-09-29 21:19:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vi du 1</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body style="background-color:rgb(203, 231, 235);">
+    
+   
+    <h1 style="font-size:60px;color:rgb(0, 140, 255);background-color:rgb(233, 203, 198);font-family:verdana;text-align: center;">Wecome To Lê Loan Entertainment</h1>
+    <h2><a href="https://www.facebook.com/loan.lethi.589583">Link Facebook : <mark>Lê Loan</mark></a><h2>
+    <p style="color:rgba(255, 0, 76, 0.651);font-family:courier;font-size: 160%" > Hình ảnh Cute Hạt Me : <q>Đây là Lê Loan. Một cô gái rất dễ thương !</q></p>
+    
+    <img src="https://scontent-sin6-4.xx.fbcdn.net/v/t1.6435-9/182428518_547424292911195_7624645850985743180_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=byKA4K09twYAX9B4Z-w&_nc_ht=scontent-sin6-4.xx&oh=6472e1890cd18feecaa371c894fdfc63&oe=61788B87" alt="Ảnh Lê Loan" width="400" height="600">
+    <img src="https://scontent.fhph1-1.fna.fbcdn.net/v/t1.6435-9/126050705_439914373833893_5668672422449441561_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=e3f864&_nc_ohc=zqaPU7dOOAUAX8eOQgA&_nc_ht=scontent.fhph1-1.fna&oh=a32bdf61003459c7f819bf5beec5c700&oe=61758901"alt="Ảnh Lê Loan" width="530"height="600">
+   <img src ="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-9/131458411_457304072094923_16016642176093948_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=_gEb39A5eMcAX8YHAMw&_nc_ht=scontent-hkg4-1.xx&oh=044674a1f6f81581f6c324b4d408e0d9&oe=61799286 " alt = "Ảnh Lê Loan" width = "480" height="600">
+    <img src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-9/86970534_246042766554389_3471325210102202368_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_ohc=zXQtis2OobAAX8jrAL1&_nc_ht=scontent-hkg4-1.xx&oh=a4423fff5f8a0adb4965011c0e60ecc9&oe=6178A3F0"alt = "Ảnh Lê Loan" width = "450" height="600">
+    <address>
+        Written by Mr PHAP.<br> 
+        Thanks...
+    </address>
+</body>
+
+</html>
